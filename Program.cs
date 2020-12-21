8fc8972ef0d925440635afcb89bfb47ee7cbbad3
Mykola Kachanov <kachanov.mykola@vu.cdu.edu.ua>
2020-12-21 15:03:40 +0200

    Створено найпростіший консольний додаток

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab6_MTZKIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
